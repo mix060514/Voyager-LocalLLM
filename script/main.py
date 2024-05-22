4e0d9fbb780931765ac3e39b96a329e2fa0aec8f
mix060514 <mix060514@gmail.com>
2024-05-22 09:29:15 +0800

    move main.py to script

--- script/main.py
+++ script/main.py
@@ -3,18 +3,17 @@ from voyager import Voyager
 if __name__ == '__main__':
     openai_api_key = "dummy"
 
-
     # First instantiate Voyager with skill_library_dir.
     voyager = Voyager(
         mc_port=59341,
         openai_api_key=openai_api_key,
-        # skill_library_dir="ckpt_dir2", # Load a learned skill library.
-        ckpt_dir="ckpt_dir_auto_1", # Feel free to use a new dir. Do not use the same dir as skill library because new events will still be recorded to ckpt_dir. 
+        skill_library_dir="skill_library/trial1", # Load a learned skill library.
+        ckpt_dir="ckpt_dirs/ckpt8", # Feel free to use a new dir. Do not use the same dir as skill library because new events will still be recorded to ckpt_dir. 
         resume=False, # Do not resume from a skill library because this is not learning.
-        ollama_model_name='codeqwen',
+        # resume=True,
+        ollama_model_name='codeqwen:code',
         use_ollama=True,
     )
-
     # Run task decomposition
     voyager.learn()
     # task = "Craft a word pickaxe" # e.g. "Craft a diamond pickaxe"

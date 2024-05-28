1e865fa03aff1e2012142a123fc677d45d7547e1
mix060514 <mix060514@gmail.com>
2024-05-29 00:36:23 +0800

    change default model

--- script/main.py
+++ script/main.py
@@ -5,13 +5,13 @@ if __name__ == '__main__':
 
     # First instantiate Voyager with skill_library_dir.
     voyager = Voyager(
-        mc_port=59341,
+        mc_port=33913,
         openai_api_key=openai_api_key,
-        skill_library_dir="skill_library/trial1", # Load a learned skill library.
+        # skill_library_dir="skill_library/trial1", # Load a learned skill library.
         ckpt_dir="ckpt_dirs/ckpt8", # Feel free to use a new dir. Do not use the same dir as skill library because new events will still be recorded to ckpt_dir. 
         resume=False, # Do not resume from a skill library because this is not learning.
         # resume=True,
-        ollama_model_name='codeqwen:code',
+        ollama_model_name='codeqwen',
         use_ollama=True,
     )
     # Run task decomposition
